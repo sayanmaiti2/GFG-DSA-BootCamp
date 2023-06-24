86acfc9ad6ee8bc7f396c1ab5a61ce6401df0e06
sayan <sayanmaiti31@gmail.com>
2023-06-24 08:58:36 +0530

    feat(bit-ops): update loop init

---
+++ BitMagic/CountSetBitsSum.java
@@ -0,0 +1,19 @@
+package BitMagic;
+
+import java.util.Scanner;
+
+public class CountSetBitsSum {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number:");
+            int num = sc.nextInt();
+            int result = countSetBitsTrial(num);
+            System.out.printf("The number of set bits in %d is %d", num, result);
+        }
+    }
+
+
+    static countSumOfSetBits(int n) {
+        
+    }
+}
